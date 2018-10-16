948c553193d5423fb5c84664a1092b87c6921865
stev0531 <stev0531@morris.umn.edu>
2018-10-16 11:33:50 -0500

    Create `EchoClient.java` and `EchoServer.java`
    We started the outline for the code. It compiled
    when we did it manually but none of the tests pass.

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,4 +1,31 @@
 package echoserver;
+import java.net.*;
+import java.io.*;
 
 public class EchoServer {
+  public static final int portNumber = 6013;
+
+  public static void main(String[] args) {
+    try {
+      ServerSocket sock = new ServerSocket(portNumber);
+
+      while(true){
+        Socket client = sock.accept();
+
+        InputStream input = client.getInputStream();
+        OutputStream output = client.getOutputStream();
+
+        int line;
+        while((line = input.read()) != -1) {
+          output.write(line);
+        }
+
+        client.close();
+      }
+
+    } catch (IOException ioe) {
+      System.out.println("We caught an unexpected exception");
+      System.err.println(ioe);
+    }
+  }
 }
